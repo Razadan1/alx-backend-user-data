157f4c3adadd91e040d3a5d4bb136cb5d2acab19
Razadan1 <danmoleomomayowa@gmail.com>
2024-11-18 13:52:57 +0100

    Updated code

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,8 +8,7 @@ import os
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-    """
+    """ Redacting Formatter class """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
@@ -25,26 +24,12 @@ class RedactingFormatter(logging.Formatter):
                             super().format(record), self.SEPARATOR)
 
 
-PII_FIELDS = ("name", "email", "password", "ssn", "phone")
-
-
-def get_db() -> mysql.connector.connection.MYSQLConnection:
-    """ Connection to MySQL environment """
-    db_connect = mysql.connector.connect(
-        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
-        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
-        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        database=os.getenv('PERSONAL_DATA_DB_NAME')
-    )
-    return db_connect
-
-
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """ Returns regex obfuscated log messages """
     for field in fields:
-        message = re.sub(f'{field}=(.*?){separator}',
-                         f'{field}={redaction}{separator}', message)
+        pattern = rf'{field}=[^;]+(?={separator}|$)'
+        message = re.sub(pattern, f'{field}={redaction}', message)
     return message
 
 
@@ -57,16 +42,26 @@ def get_logger() -> logging.Logger:
     target_handler = logging.StreamHandler()
     target_handler.setLevel(logging.INFO)
 
-    formatter = RedactingFormatter(list(PII_FIELDS))
-    target_handle.setFormatter(formatter)
+    formatter = RedactingFormatter(["password", "date_of_birth"])
+    target_handler.setFormatter(formatter)
 
     logger.addHandler(target_handler)
     return logger
 
 
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Connection to MySQL environment """
+    return mysql.connector.connect(
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME')
+    )
+
+
 def main() -> None:
     """ Obtain database connection using get_db
-    retrieve all role in the users table and display
+    retrieve all roles in the users table and display
     each row under a filtered format
     """
     db = get_db()
@@ -77,10 +72,8 @@ def main() -> None:
     logger = get_logger()
 
     for row in cursor:
-        info_answer = ''
-        for f, p in zip(row, headers):
-            info_answer += f'{p}={(f)}; '
-        logger.info(info_answer)
+        message = "; ".join(f"{header}={value}" for header, value in zip(headers, row)) + ";"
+        logger.info(message)
 
     cursor.close()
     db.close()
